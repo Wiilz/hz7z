118c2a29d4239fd6457ec883b3f10cc54a8ea8fa
Wiilz <rayleignze@gmail.com>
2020-05-06 18:18:01 +0800

    feat(server): create tornado server to run flask app

---
+++ server.py
@@ -0,0 +1,12 @@
+from tornado.httpserver import HTTPServer
+from tornado.wsgi import WSGIContainer
+from tornado.ioloop import IOLoop
+from run import app
+
+# 仅 win + py38 时需添加以下两行
+import asyncio
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(5001)
+IOLoop.instance().start()
