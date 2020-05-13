813ab2a9216d55250ec4a4237d4bf1614ce9b8d2
Wiilz <rayleignze@gmail.com>
2020-05-13 19:41:49 +0800

    fix(start.sh): update tornado to gevent

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+server="gevent_server.py"
+# server="tornado_server.py"
 case "$@" in
 start)
-  $(python server.py >>tornado.logs 2>&1 &)
+  $(python ${server} >>server.logs 2>&1 &)
   ;;
 stop)
   kill -9 $(ps aux | grep python | grep -v grep | awk '{print $1}' | xargs)
@@ -10,7 +12,7 @@ stop)
 restart)
   kill -9 $(ps aux | grep python | grep -v grep | awk '{print $1}' | xargs)
   sleep 1
-  $(python server.py >>tornado.logs 2>&1 &)
+  $(python ${server} >>server.logs 2>&1 &)
   ;;
 *)
   echo 'unknown arguments args(start|stop|restart)'
